8d2ae1070d2b3ec2348b09a6b38aef745aee65ca
Philip Lundström <philip.lundstrom@live.se>
2024-10-17 14:34:26 +0200

    Restructe into extTello

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="extTello",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "djitellopy<=2.4.0",
+    ],
+    author="Philip Lundström",
+    author_email="philip.lundstrom@live.se",
+    description="Extension of djitellopy",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/my_package",  # URL for your package
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
+
