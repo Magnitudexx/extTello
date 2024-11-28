e7a94eec3a4684c6ce43aa35f6196d6fe18974d2
Philip Lundström <philip.lundstrom@live.se>
2024-11-28 15:34:21 +0100

    General fixes

--- extTello/extTello.py
+++ extTello/extTello.py
@@ -6,6 +6,7 @@ import math
 import threading
 import time
 from typing import Callable, Dict
+from types import MethodType
 
 class extTello(Tello):
     def __init__(self, x=0.0, y=0.0, z=0.0, theta=0.0):
@@ -17,6 +18,8 @@ class extTello(Tello):
         self.auto_controller_thread = None
         self.lock = threading.Lock()
         self.frame = None
+        self.mask = None
+        self.have_identifier = False
 
     def StateUpdater(self):
         lt = time.time()
@@ -43,27 +46,34 @@ class extTello(Tello):
 
 
     def travel_path(self, wps, max_speed, acc):
+        idn = getattr(self, "object_identifier", lambda: None)
         for i in range(len(wps) - 1):
             start = wps[i]
             end = wps[i + 1]
             
             # Compute distance and direction to next waypoint
             dist = self.__distance(start, end)
-            dir_x, dir_y, dir_z = (end[0] - start[0]) / dist, (end[1] - start[1]) / dist, (end[2] - start[2]) / dist
+            #dir_x, dir_y, dir_z = (end[0] - start[0]) / dist, (end[1] - start[1]) / dist, (end[2] - start[2]) / dist
+            dir_x, dir_y = (end['x'] - start['x']) / dist, (end['y'] - start['y']) / dist
             
             # Get speed profile for this segment
             speed_profile = self.__calc_speed_profile(float(dist), acc, max_speed)
             # Move self along speed profile
             for speed in speed_profile:
-                self.send_rc_control(int(speed * dir_x), int(speed * dir_y), int(speed * dir_z), 0)
-                time.sleep(0.1)
+                if idn() is not None:
+                    self.send_rc_control(0,0,0,0)
+                    self.start_auto_controller()
+                    return
+                self.send_rc_control(int(speed * dir_x), int(speed * dir_y), 0, 0)
+                time.sleep(0.05)
             
             # Stop between wps to stabilize
             self.send_rc_control(0, 0, 0, 0)
             time.sleep(2)
-    def start_auto_controller(self, func: Callable[['extTello'], Dict | None]):
-        self.auto_controller_thread = threading.Thread(target=self.__auto_controller, args=(func,))
-        self.auto_controller_thread.start()
+    def start_auto_controller(self):
+        if self.have_identifier:
+            self.auto_controller_thread = threading.Thread(target=self.__auto_controller)
+            self.auto_controller_thread.start()
 
     def stop(self):
         self.running = False
@@ -77,29 +87,37 @@ class extTello(Tello):
         self.running = True
         self.StateUpdaterThread.start()
         time.sleep(3)
+        """
         while self.state['z'] < 110:
             self.send_rc_control(0,0,10,0)
             time.sleep(0.05)
         self.send_rc_control(0,0,0,0)
         time.sleep(1)
-    def __auto_controller(self,func: Callable[['extTello'], Dict | None]):
+        """
+    def create_object_identifier(self, func):
+        self.have_identifier = True
+        setattr(self, "object_identifier", MethodType(func, self))
+
+    def __auto_controller(self):
+        func = getattr(self, 'object_identifier', lambda: None)
         while self.running:
             with self.lock:
                 pos = self.state
-            target = func(self)
+            target = func()
             if target is None:
                 continue
-            if 'z' not in target:
-                target['z'] = pos['z']
+            #if 'z' not in target:
+                #target['z'] = pos['z']
             dist = self.__distance(pos, target)
-            if dist <= 10:
+            logging.debug(f"Dist = {dist}")
+            if dist <= 5:
                 self.send_rc_control(0,0,0,0)
-                logging.debug(f"Dist = {dist}")
                 time.sleep(0.05)
             else:
                 dir_x = (target['x'] - pos['x']) / dist
                 dir_y = (target['y'] - pos['y']) / dist
-                speeds = [int(x) if x >= 20 else 0 for x in [30*dir_x,30*dir_y]]
+                speed = 15
+                speeds = [int(x) if abs(x) >= 10 else 0 for x in [speed*dir_x, speed*dir_y]]
                 logging.debug(f"RC-speeds = {speeds}")
 
                 self.send_rc_control(speeds[0], speeds[1], 0, 0)
@@ -120,8 +138,9 @@ class extTello(Tello):
         else:
             # Long dist case: Accelerate, then maintain speed, then decelerate
             return [float(acc*t) for t in np.arange(0, time_to_max_v, 0.1)] + \
-                   [max_v for t in np.arange(time_to_max_v, dist / max_v, 0.1)] + \
+                   [max_v for _ in np.arange(time_to_max_v, dist / max_v, 0.1)] + \
                    [float(max_v - acc * (t - dist / max_v)) for t in np.arange(dist / max_v, time_to_max_v + dist / max_v, 0.1)]
 
     def __distance(self, point1, point2):
-        return np.sqrt((point1['x'] - point2['x']) ** 2 + (point1['y'] - point2['y']) ** 2 + (point1['z'] - point2['z']) ** 2)
+        #return np.sqrt((point1['x'] - point2['x']) ** 2 + (point1['y'] - point2['y']) ** 2 + (point1['z'] - point2['z']) ** 2)
+        return np.sqrt((point1['x'] - point2['x']) ** 2 + (point1['y'] - point2['y']) ** 2 )
